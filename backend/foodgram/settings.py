aaf5ef8e7c135f7440f70a596d978db68ce6f5fb
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-04-25 15:20:40 +0300

    добавил приложения

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'user.apps.UserConfig',
+    'post.apps.PostConfig',
 ]
 
 MIDDLEWARE = [
