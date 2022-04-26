31b65b5cc9865441579af4f01480aa67b4109321
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-04-26 11:41:15 +0300

    добавил цвета в тег, накатал новых вопросов

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,33 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
 
 
-class Recipe(models.Model):
-    author = models.ForeignKey(
-        "User",
-        verbose_name='Автор'
-    )
-    name = models.CharField('Название рецепта', max_length=200)
-    image = models.ImageField('Фото')
-    text = models.TextField('Описание')
-    ingredients =
-    tags =
-    cooking_time = models.IntegerField('Время в минутах')
-
-    class Meta:
-        verbose_name = 'Рецепт'
-        verbose_name_plural = 'Рецепты'
-
-    def __str__(self):
-        return self.name
-
-
 class Teg(models.Model):
+    RED = 'ff0000'
+    BLUE = '0037ff'
+    GREEN = '37ff00'
+    YELLOW = 'fffb00'
+    PURPLE = 'e600ff'
+
+    color_choise = [
+        (RED, 'Красный'),
+        (BLUE, 'Синий'),
+        (GREEN, 'Зеленый'),
+        (YELLOW, 'Желтый'),
+        (PURPLE, 'Фиолетовый'),
+    ]
+
     name = models.CharField('Тег', max_length=200)
-    color = models.CharField('Цвет')
+    color = models.CharField(
+        'Цвет в HEX',
+        unique=True,
+        choices=color_choise
+    )
     slug = models.SlugField('Слаг', unique=True)
 
     class Meta:
@@ -49,3 +48,38 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+        
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        """related_name=""",
+        verbose_name='Автор'
+    )
+    name = models.CharField('Название рецепта', max_length=200)
+    image = models.ImageField('Фото', )
+    text = models.TextField('Описание')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        """related_name=""",
+        verbose_name='Ингредиенты'
+    )
+    tags =
+    cooking_time = models.PositiveIntegerField(
+        'Время в минутах',
+        validators=[MinValueValidator(1, 'Минимальное время 1 минута')]
+    )
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+# Разобраться с картинкой
+# разобраться с релейт нейм
+# разобраться с tags recipe
