3cd71092be6bb99f7768a076f0f788157dcb69ad
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-04-26 15:40:52 +0300

    add models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Tag, Recipe, Ingredient
+# Подписки / избранное / список покупок
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('__all__')
+    search_fields = ('name,')
+    empty_value_display = '-empty-'
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+    search_fields = ('author', 'name', 'tags')
+    ordering = ('-id',)
+    empty_value_display = '-empty-'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    empty_value_display = '-empty-'
