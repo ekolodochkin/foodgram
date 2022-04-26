168fbfd4f81e7be3db5179138d8c1b538ad2cae7
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-04-26 12:28:24 +0300

    related_name ok image ok

--- backend/post/models.py
+++ backend/post/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class Teg(models.Model):
+class Tag(models.Model):
     RED = 'ff0000'
     BLUE = '0037ff'
     GREEN = '37ff00'
@@ -27,7 +27,7 @@ class Teg(models.Model):
         unique=True,
         choices=color_choise
     )
-    slug = models.SlugField('Слаг', unique=True)
+    slug = models.SlugField('Слаг', unique=True, max_length=50)
 
     class Meta:
         verbose_name = 'Тег'
@@ -40,7 +40,7 @@ class Teg(models.Model):
 class Ingredient(models.Model):
     name = models.CharField('Ингредиент', max_length=200)
     amount = models.IntegerField('Количество')
-    measurement_unit = models.CharField('Ед.измерения')
+    measurement_unit = models.CharField('Ед.измерения веса')
 
     class Meta:
         verbose_name = 'Ингредиент'
@@ -54,18 +54,22 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        """related_name=""",
+        related_name='recipes',
         verbose_name='Автор'
     )
     name = models.CharField('Название рецепта', max_length=200)
-    image = models.ImageField('Фото', )
-    text = models.TextField('Описание')
+    image = models.ImageField('Фото', upload_to='image_recipes/')
+    text = models.TextField('Описание', max_length=1000)
     ingredients = models.ManyToManyField(
         Ingredient,
-        """related_name=""",
+        related_name='recipes',
         verbose_name='Ингредиенты'
     )
-    tags =
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Теги'
+    )
     cooking_time = models.PositiveIntegerField(
         'Время в минутах',
         validators=[MinValueValidator(1, 'Минимальное время 1 минута')]
@@ -78,8 +82,3 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
-
-
-# Разобраться с картинкой
-# разобраться с релейт нейм
-# разобраться с tags recipe
