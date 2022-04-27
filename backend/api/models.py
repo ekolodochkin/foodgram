c1ef1fc6c394d5266dc23842b937bfaee55b3af1
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-04-27 18:54:25 +0300

    related name

--- backend/api/models.py
+++ backend/api/models.py
@@ -135,11 +135,13 @@ class Follow(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        related_name='following',
         verbose_name='Автор',
     )
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        related_name='follower'
         verbose_name='подписчик',
     )
 
