f0d52bfb0e51e3b6dc99b195b78f5e9fa085a5c0
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-02 12:09:02 +0300

    комм

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
 ]
 
 # Юзер (get post)
-# http://localhost/api/users/
 # http://localhost/api/users/{id}/
+# Djoser
+# http://localhost/api/users/
 # http://localhost/api/users/me/
 # http://localhost/api/users/set_password/
 # http://localhost/api/auth/token/login/
 # http://localhost/api/auth/token/logout/
-# get post
 
 # Подписки (get, post, del)
 # http://localhost/api/users/subscriptions/
