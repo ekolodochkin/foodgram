deb537715779d95c0115b984dec67a99244edd13
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-02 10:52:03 +0300

    раскидываем пути

---
+++ backend/api/urls.py
@@ -0,0 +1,26 @@
+# Рецепты (get post patch del)
+# http://localhost/api/recipes/
+# http://localhost/api/recipes/{id}/
+#
+#
+#
+#Теги (get)
+# http://localhost/api/tags/
+# http://localhost/api/tags/{id}/
+#
+# Список покупок(get post del)
+# http://localhost/api/recipes/download_shopping_cart/
+# http://localhost/api/recipes/{id}/shopping_cart/
+#
+#
+#
+#
+# Избранное (post del)
+# http://localhost/api/recipes/{id}/favorite/
+
+#
+#
+# Ингредиенты (get)
+# http://localhost/api/ingredients/
+# http://localhost/api/ingredients/{id}/
+#
---
+++ backend/user/urls.py
@@ -0,0 +1,13 @@
+# Юзер (get post)
+# http://localhost/api/users/
+# http://localhost/api/users/{id}/
+# http://localhost/api/users/me/
+# http://localhost/api/users/set_password/
+# http://localhost/api/auth/token/login/
+# http://localhost/api/auth/token/logout/
+# get post
+
+# Подписки (get, post, del)
+# http://localhost/api/users/subscriptions/
+# http://localhost/api/users/{id}/subscribe/
+#
