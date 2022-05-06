a398f6fe4c5663e989bcb93a883d322e75cd3e66
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-06 16:01:32 +0300

    вью с несколькими сериализаторами

--- backend/user/views.py
+++ backend/user/views.py
@@ -1,36 +1,63 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import filters, permissions, status, views, viewsets
+from rest_framework import mixins, filters, permissions, status, views, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAdminUser
 from django.contrib.auth import get_user_model
 from .serializers import UserRegSerializers, UserSerializers
 from api.pagination import MyPagination
+from djoser.serializers import SetPasswordSerializer
+from .permissions import ForAuthUserOrAllowAny
+
 
 
 User = get_user_model()
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class CreateRetrieveListViewSet(mixins.CreateModelMixin,
+                                mixins.ListModelMixin,
+                                mixins.RetrieveModelMixin,
+                                viewsets.GenericViewSet):
+    pass
+
+
+class UserViewSet(CreateRetrieveListViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializers
     pagination_class = MyPagination
-    # permission_classes = (IsAdminUser,)
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ['username', 'email']
+    permission_classes = [ForAuthUserOrAllowAny]
+    serializers = {
+        'create': UserRegSerializers,
+        'me': UserSerializers,
+        'set_password': SetPasswordSerializer,
+    }
 
-    # @action(detail=False, methods=['get', 'patch'],
-    #         permission_classes=[permissions.IsAuthenticated])
-    # def me(self, request):
-    #     if request.method == 'GET':
-    #         serializer = self.get_serializer(request.user)
-    #         return Response(data=serializer.data)
-    #     if request.method == 'PATCH':
-    #         serializer = self.get_serializer(
-    #             request.user,
-    #             data=request.data,
-    #             partial=True
-    #         )
-    #         serializer.is_valid(raise_exception=True)
-    #         serializer.save(role=request.user.role)
-    #         return Response(data=serializer.data)
+    def get_serializer_class(self):
+        try:
+            return self.serializers[self.action]
+        except KeyError:
+            return self.serializer_class
+
+    @action(
+        methods=['GET'],
+        detail=False,
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK
+        )
+
+    @action(
+        methods=['POST'],
+        detail=False,
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    def set_password(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.request.user.set_password(
+            serializer.validated_data.get('new_password')
+        )
+        self.request.user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
