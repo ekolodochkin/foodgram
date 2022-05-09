1140974286e7204f97d1e3c3e0aef99e0c373796
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-09 16:50:02 +0300

    add new viewset

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,23 @@
 from rest_framework.response import Response
 from rest_framework import mixins, permissions, status, viewsets
 from rest_framework.decorators import action
-from .models import Tag
-from .serializers import TagSerializers
-from api.pagination import MyPagination
+from .models import Tag, Ingredient, Recipe
+from .serializers import TagSerializers, IngredientSerializers, RecipeSerializers
+from .pagination import MyPagination
 # from .permissions import 
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializers
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    pagination_class = MyPagination
+    serializer_class = RecipeSerializers
