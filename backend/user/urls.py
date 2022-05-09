389021c88bddf7d431b28e20a056657008aef8b6
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-09 20:49:21 +0300

    del comments

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -11,17 +11,3 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken'))
 ]
-
-# Юзер (get post)
-# http://localhost/api/users/{id}/
-# Djoser
-# http://localhost/api/users/
-# http://localhost/api/users/me/
-# http://localhost/api/users/set_password/
-# http://localhost/api/auth/token/login/
-# http://localhost/api/auth/token/logout/
-
-# Подписки (get, post, del)
-# http://localhost/api/users/subscriptions/
-# http://localhost/api/users/{id}/subscribe/
-#
