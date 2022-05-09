c82ec03e8ae9416846d148c70780c9ebff858d14
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-09 11:42:42 +0300

    add tag vievs

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import mixins, permissions, status, viewsets
+from rest_framework.decorators import action
+from .models import Tag
+from .serializers import TagSerializers
+from api.pagination import MyPagination
+# from .permissions import 
 
-# Create your views here.
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializers
