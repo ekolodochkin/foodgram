8a0eb64db2812110f70d8ed8c78cbf3a7ef9d1d1
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-11 16:29:19 +0300

    add Cors

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -29,11 +29,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -93,6 +95,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 STATIC_URL = '/static/'
