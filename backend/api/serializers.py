ca903b2e1f1ca26b9ce3e175c82e483b673403fa
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-11 18:09:54 +0300

    add def update

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -102,11 +102,20 @@ class RecipeSerializers(serializers.ModelSerializer):
         self.create_ingredients(ingredients_data, recipe)
         return recipe
 
-    
     def update(self, instance, validated_data):
-        return
-
-
+        instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get(
+            'cooking_time', instance.cooking_time
+        )
+        instance.tags.clear()
+        tags_data = self.initial_data.get('tags')
+        instance.tags.set(tags_data)
+        IngredientAmount.objects.filter(recipe=instance).all().delete()
+        self.create_ingredients(validated_data.get('ingredients'), instance)
+        instance.save()
+        return instance
 
 
 class RecipeForFollowSerializer(serializers.ModelSerializer):
