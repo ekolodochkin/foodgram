8d8d2fa9d07797e8f96896a00987aa334eb0ee02
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-11 16:29:00 +0300

    del comm

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,22 +17,17 @@ urlpatterns = [
 # Рецепты (get post patch del)
 # http://localhost/api/recipes/
 # http://localhost/api/recipes/{id}/
-#
-#
-#
-#
+
+
 # Список покупок(get post del)
 # http://localhost/api/recipes/download_shopping_cart/
 # http://localhost/api/recipes/{id}/shopping_cart/
-#
-#
-#
-#
+
+
 # Избранное (post del)
 # http://localhost/api/recipes/{id}/favorite/
 
-#
-#
+
 # Ингредиенты (get)
 # http://localhost/api/ingredients/
 # http://localhost/api/ingredients/{id}/
