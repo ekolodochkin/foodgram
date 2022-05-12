ab959d98890b3dbe01c9373113de1c38b008f965
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-12 16:56:10 +0300

    убрал длинну ед измер

--- backend/api/models.py
+++ backend/api/models.py
@@ -41,7 +41,7 @@ class Ingredient(models.Model):
     """ -- Ингредиенты -- """
 
     name = models.CharField('Ингредиент', max_length=200)
-    measurement_unit = models.CharField('Ед.измерения веса', max_length=3)
+    measurement_unit = models.CharField('Ед.измерения веса')
 
     class Meta:
         verbose_name = 'Ингредиент'
