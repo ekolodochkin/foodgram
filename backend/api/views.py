8d7779dc5042cddc04fe35e62e9e20236081a56a
Jeka-SPB <evgenykolodochkin@yandex.ru>
2022-05-15 18:14:37 +0300

    add import, perform_create, def favorite

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,12 @@
 from rest_framework import mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
-from .models import Ingredient, Recipe, Tag
+from rest_framework.permissions import IsAuthenticated
+from .models import Ingredient, Recipe, Tag, Favorite
 from .pagination import MyPagination
 from .serializers import (IngredientSerializers, RecipeSerializers,
-                          TagSerializers)
+                          TagSerializers, PartRecipeSerializers)
+from django.shortcuts import get_object_or_404
 
 # from .permissions import 
 
@@ -25,24 +26,45 @@ class RecipeViewSet(viewsets.ModelViewSet):
     pagination_class = MyPagination
     serializer_class = RecipeSerializers
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
     # @action(
     #     methods=[],
     #     detail=,
-    #     permission_classes=[],
+    #     permission_classes=[IsAuthenticated],
     # )
     # def download_shopping_cart(self, request):
 
     # @action(
-    #     methods=[],
-    #     detail=,
-    #     permission_classes=[],
+    #     methods=['GET', 'DELETE'],
+    #     detail=True,
+    #     permission_classes=[IsAuthenticated],
     # )
-    # def shopping_cart(self, request, id):
+    # def shopping_cart(self, request, pk):
 
-    # @action(
-    #     methods=[],
-    #     detail=,
-    #     permission_classes=[],
-    # )
-    # def favorite(self, request, id):
+
+    @action(
+        methods=['GET', 'DELETE'],
+        detail=True,
+        permission_classes=[IsAuthenticated],
+    )
+    def favorite(self, request, pk):
+        if request.method == 'GET':
+            if Favorite.objects.filter(user=request.user, recipe__id=pk).exists():
+                return Response({
+                'errors': 'Рецепт уже добавлен в избранное'
+            }, status=status.HTTP_400_BAD_REQUEST)
+            recipe = get_object_or_404(Recipe, id=pk)
+            Favorite.objects.create(user=request.user, recipe=recipe)
+            serializer = PartRecipeSerializers(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        elif request.method == 'DELETE':
+            favorite = Favorite.objects.filter(user=request.user, recipe__id=pk)
+            if favorite.exists():
+                favorite.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({
+                'errors': 'Рецепт уже удален'
+            }, status=status.HTTP_400_BAD_REQUEST)
 
